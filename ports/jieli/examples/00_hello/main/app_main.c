39f76b9c0f6378ef1285ed540d8a6901c07b335f
whstart <1065510910@qq.com>
2022-11-11 19:28:15 +0800

    refactor: 删除不必要的函数

--- ports/jieli/examples/00_hello/main/app_main.c
+++ ports/jieli/examples/00_hello/main/app_main.c
@@ -1,10 +1,3 @@
-/*********************************************************************************************
-    *   Filename        : app_main.c
-
-    *   Description     :
-
-    *   Copyright:(c)JIELI  2011-2019  @ , All Rights Reserved.
-*********************************************************************************************/
 #include "system/includes.h"
 #include "app_config.h"
 #include "app_action.h"
@@ -25,7 +18,6 @@
 #define LOG_ERROR_ENABLE
 #define LOG_DEBUG_ENABLE
 #define LOG_INFO_ENABLE
-/* #define LOG_DUMP_ENABLE */
 #define LOG_CLI_ENABLE
 #include "debug.h"
 
@@ -64,7 +56,6 @@ const struct task_info task_info_table[] = {
 };
 
 APP_VAR app_var;
-
 void app_var_init(void)
 {
     app_var.play_poweron_tone = 1;
@@ -74,63 +65,19 @@ void app_var_init(void)
     app_var.poweroff_tone_v = 330;
 }
 
-
-__attribute__((weak))
-u8 get_charge_online_flag(void)
-{
-    return 0;
-}
+// TODO: 避免SDK编译报错, 后期解决.
 void ble_module_enable (u8 en)
 {
-    // 避免SDK编译报错, 后期解决.
-}
-void clr_wdt(void);
-void check_power_on_key(void)
-{
-#if TCFG_POWER_ON_NEED_KEY
 
-    u32 delay_10ms_cnt = 0;
-    while (1) {
-        clr_wdt();
-        os_time_dly(1);
-
-        extern u8 get_power_on_status(void);
-        if (get_power_on_status()) {
-            log_info("+");
-            delay_10ms_cnt++;
-            if (delay_10ms_cnt > 70) {
-                /* extern void set_key_poweron_flag(u8 flag); */
-                /* set_key_poweron_flag(1); */
-                return;
-            }
-        } else {
-            log_info("-");
-            delay_10ms_cnt = 0;
-            log_info("enter softpoweroff\n");
-            power_set_soft_poweroff();
-        }
-    }
-#endif
-}
-
-void board_pre_init(void)
-{
-    clk_set("sys", BT_NORMAL_HZ);
-    u32 sys_clk =  clk_get("sys");
-    bt_pll_para(TCFG_CLOCK_OSC_HZ, sys_clk, 0, 0);
 }
 
 void app_main()
 {
-    board_pre_init();
     while(1){
         log_info ("hello_world");
         os_time_dly(100);
     }
-
 }
-
-
 
 int eSystemConfirmStopStatus(void)
 {
@@ -140,11 +87,4 @@ int eSystemConfirmStopStatus(void)
     /* log_info("100ms wakeup"); */
     return 1;
 }
-
-__attribute__((used)) int *__errno()
-{
-    static int err;
-    return &err;
-}
-
 
